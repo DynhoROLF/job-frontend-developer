371b20156d988746a632fe14107f056288bc6f0a
DynhoROLF <dynhoromero@gmail.com>
2023-11-14 01:39:29 -0300

    feat: main page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,24 +1,59 @@
 'use client'
-import Header from '@/components/Header'
-import { SearchIcon } from '@chakra-ui/icons'
-import { YoutubeChannel } from '@/components/YoutubeChannel'
 
-import { ChakraProvider, Flex, IconButton, Input } from '@chakra-ui/react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+import Footer from '@/components/Footer'
+import { Button, Flex, Input, Text } from '@chakra-ui/react'
 
 export default function Home() {
-  return (
-    <ChakraProvider>
-      <Header />
+  const router = useRouter()
+  const [bandName, setBandName] = useState('')
 
-      <Flex className="h-screen bg-zinc-900">
-        <Input
-          placeholder="Pesquisar"
-          color={'lightblue'}
-          className="flex flex-co"
-        />
-        <IconButton aria-label="Search database" icon={<SearchIcon />} />
-        {/* <YoutubeChannel username="pewdiepie" /> */}
+  const handleInputChange = (event: any) => {
+    setBandName(event.target.value)
+  }
+
+  const handleSearch = () => {
+    if (bandName) {
+      router.push(`/channel/${bandName}`)
+    } else {
+      alert('Preencha o campo de pesquisa')
+    }
+  }
+
+  return (
+    <>
+      <Flex className="h-screen bg-zinc-900 flex-col justify-center items-center">
+        <Flex className="flex-col items-center mt-[-10rem]">
+          <Text as="b" fontSize="6xl" color="gray.100">
+            YourTube
+          </Text>
+        </Flex>
+        <Flex className="flex-col items-center p-2">
+          <Input
+            placeholder="Nome da banda"
+            color={'white'}
+            focusBorderColor="red.400"
+            isRequired
+            width="20rem"
+            value={bandName}
+            onChange={handleInputChange}
+            borderRadius="full"
+          />
+          <Button
+            aria-label="Search database"
+            className="mt-2"
+            width="20rem"
+            colorScheme="red"
+            onClick={handleSearch}
+            borderRadius="full"
+          >
+            PESQUISAR
+          </Button>
+        </Flex>
       </Flex>
-    </ChakraProvider>
+      <Footer />
+    </>
   )
 }
